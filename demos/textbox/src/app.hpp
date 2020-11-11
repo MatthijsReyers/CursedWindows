7506915633cd1f32dafc5a41b00c12d2442f1aa1
MatthijsReyers <matthijscrafter@gmail.com>
2020-11-11 01:58:11 +0100

    Fixed resizing bug in demo

--- demos/textbox/src/app.hpp
+++ demos/textbox/src/app.hpp
@@ -1,5 +1,6 @@
 
 #include <cursedwindows/application.hpp>
+#include <cursedwindows/layouts/grid.hpp>
 #include <cursedwindows/layouts/border.hpp>
 
 #include <memory>
@@ -17,11 +18,20 @@ protected:
 TextBoxApp::TextBoxApp()
     :cw::application()
 {
-    // Create bordered layout as main layout.
-    this->setMainWidget(std::make_shared<cw::BorderLayout>());
+    // Create grid layout as main layout.
+    auto main = std::make_shared<cw::GridLayout>(2,2,0,0);
+    this->setMainWidget(main);
+
+    main->setVerticalGap(1);
+
+    // Add bordered layouts to grid.
+    main->addWidget(std::make_shared<cw::BorderLayout>(), 0, 0);
+    main->addWidget(std::make_shared<cw::BorderLayout>(), 0, 1);
+    main->addWidget(std::make_shared<cw::BorderLayout>(), 1, 0);
+    main->addWidget(std::make_shared<cw::BorderLayout>(), 1, 1);
 }
 
 void TextBoxApp::run()
 {
-    
+
 }
